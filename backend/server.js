9fe1f23fad56fea984055cc64eabfd23d23f4220
BibekKoirala07 <www.koiralabibek2058@gmail.com>
2025-04-07 15:01:28 +0545

    done

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", process.env.PROD_FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
+
+console.log("corsOption", corsOptions);
 
 const taskRoutes = require("./routes/tasksRoutes");
 const userRoutes = require("./routes/userRoutes");
